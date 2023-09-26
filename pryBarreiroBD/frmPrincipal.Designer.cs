2983222c3d5ca394048ecf3cff001e9738e6bfa8
Manuelbarreiro03 <manuelbarreiro2003@gmail.com>
2023-09-26 14:29:56 -0300

    26/09/2023

---
+++ pryBarreiroBD/frmPrincipal.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace pryBarreiroBD
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.cmdConectarBase = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmdConectarBase
+            // 
+            this.cmdConectarBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdConectarBase.Location = new System.Drawing.Point(12, 12);
+            this.cmdConectarBase.Name = "cmdConectarBase";
+            this.cmdConectarBase.Size = new System.Drawing.Size(110, 40);
+            this.cmdConectarBase.TabIndex = 0;
+            this.cmdConectarBase.Text = "Conectar";
+            this.cmdConectarBase.UseVisualStyleBackColor = true;
+            this.cmdConectarBase.Click += new System.EventHandler(this.cmdConectarBase_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(12, 65);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(123, 25);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "-Resultado-";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 212);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.cmdConectarBase);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.Text = "acceso base de datos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdConectarBase;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
---
+++ pryBarreiroBD/frmPrincipal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Odbc;
+using System.Data.OleDb;
+
+namespace pryBarreiroBD
+{
+    public partial class frmPrincipal : Form
+    {
+        OleDbConnection ConexionBD;
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdConectarBase_Click(object sender, EventArgs e)
+        {
+            ConexionBD = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\\Users\\Alumnos\\source\\repos\\pryBarreiroBD\\pryBarreiroBD\\BaseDatos\\EL_CLUB.accdb");
+            ConexionBD.Open();
+            lblResultado.Text = "Conectado";
+            lblResultado.BackColor = Color.LightGreen;
+        }
+    }
+}
